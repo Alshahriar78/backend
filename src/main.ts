5d74c690d8a0108527271e402785495479fb38ca
Alshahriar78 <alshahriar78@gmail.com>
2026-09-24 15:37:13 +0600

    fixed fronted cors Issues

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.enableCors({
    origin: [
     'http://localhost:5173',
+    'https://mokamtola-efootball-tournament.netlify.app/',
     'https://YOUR-NETLIFY-SITE.netlify.app',
   ],
 });
